cfdd44f9eb3c1c5fab927160ca80ef52e7f3ab1b
Hwa Rang Na <na.hwarang@gmail.com>
2019-08-21 10:34:46 +0200

    Optimise imports.

--- app/src/main/java/com/niden/unsplash/MainActivity.kt
+++ app/src/main/java/com/niden/unsplash/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.niden.unsplash
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.AttributeSet
 import android.view.Menu
 import android.view.View
 import android.widget.Button
@@ -20,7 +17,7 @@ import com.niden.unsplash.PhotosGrid.*
 class MainActivity : AppCompatActivity() {
 
     var viewState: PhotosGridViewModel? = null
-
+    
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: PhotosGridAdapter
     private lateinit var searchView: SearchView
